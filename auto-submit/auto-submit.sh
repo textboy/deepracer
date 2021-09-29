ad760c7b985eb8fbd7a300289a7314dd768b5f78
MK <textboy@126.com>
2021-09-29 18:29:55 +0800

    mask

--- auto-submit/auto-submit.sh
+++ auto-submit/auto-submit.sh
@@ -2,7 +2,7 @@ aws configure add-model --service-model file://deepracer-model.json  --service-n
 
 for i in {1..2};
 do
-    aws deepracer --endpoint-url https://deepracer-prod.us-east-1.amazonaws.com create-leaderboard-submission --model-arn "arn:aws:deepracer:us-east-1:706815244402:model/reinforcement_learning/6607ca7a-b616-4d62-ac77-c9e0b0362203" --leaderboard-arn "arn:aws:deepracer:::leaderboard/9f2d829b-888d-4fc1-ba83-215ce4c01851"
+    aws deepracer --endpoint-url https://deepracer-prod.us-east-1.amazonaws.com create-leaderboard-submission --model-arn "arn:aws:deepracer:us-east-1:<your account ID>:model/reinforcement_learning/<your model ID>" --leaderboard-arn "arn:aws:deepracer:::leaderboard/<your leaderboard ID>"
     echo $i
     sleep 600
 done
